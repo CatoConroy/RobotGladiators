0465e8baa5ccfbb6ebdaba0f854a797998b89198
Cato <catoconroy@protonmail.com>
2024-02-01 10:02:04 -0600

    Start and End Game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -89,6 +89,8 @@ for(let i = 0; i < enemyNames.length; i++) {
 
       let pickedEnemyName = enemyNames [i];
   enemyHealth = 50;  
+
+ 
   
   // debugger;
   fight(pickedEnemyName);
@@ -99,7 +101,31 @@ for(let i = 0; i < enemyNames.length; i++) {
     window.alert("You have lost the battle! Game Over!");
     break;
     }
+  } 
+  
+  // After Loop ends, call end game
+  endGame();
+}
+
+let endGame = function (){
+
+  if (playerHealth > 0){
+    window.alert("Great job, you've survived the game! You now have a score of" + playerMoney + ".0");
   }
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+  let playAgainConfirm = window.confirm ("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    startGame()
+    }
+
+    else {
+      window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+  
+
 }
 
 startGame();
